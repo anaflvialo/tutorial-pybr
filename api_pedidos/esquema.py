6be70bc1e4dad99fa4609865ad67663f9735ccb5
Ana Flavia Lopes <anaflvialo@gmail.com>
2022-03-03 15:46:01 -0300

    Adiciona documentação ao projeto

--- api_pedidos/esquema.py
+++ api_pedidos/esquema.py
@@ -1,15 +1,18 @@
 from nturl2path import url2pathname
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     sku: str
     description: str
-    image_url: str 
+    image_url: str
     reference: str
     quantity: int
+
 
 class HealthCheckResponse(BaseModel):
     status: str
 
+
 class ErrorResponse(BaseModel):
-    message:str
+    message: str
