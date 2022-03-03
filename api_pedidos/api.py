f442c6aeb0902e166d61fd2fbf1e83418aa987ae
Ana Flavia Lopes <anaflvialo@gmail.com>
2022-03-03 16:57:38 -0300

    Modifica API para responder de forma assincrona

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,5 +1,4 @@
 from http import HTTPStatus
-from uuid import UUID
 
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse
@@ -9,12 +8,29 @@ from api_pedidos.excecao import (
     FalhaDeComunicacaoError,
     PedidoNaoEncontradoError,
 )
+from api_pedidos.magalu_api_async import recuperar_itens_por_pedido
 
 app = FastAPI()
 
 
-def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
-    pass
+@app.exception_handler(PedidoNaoEncontradoError)
+async def tratar_erro_pedido_nao_encontrado(
+    request: Request, exc: PedidoNaoEncontradoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content={"message": "Pedido não encontrado"},
+    )
+
+
+@app.exception_handler(FalhaDeComunicacaoError)
+async def tratar_erro_falha_de_comunicacao(
+    request: Request, exc: FalhaDeComunicacaoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_GATEWAY,
+        content={"message": "Falha de comunicação com o servidor remoto"},
+    )
 
 
 @app.get(
@@ -45,25 +61,7 @@ async def healthcheck():
     description="Retorna todos os itens de um determinado pedido",
     response_model=list[Item],
 )
-def listar_items(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+async def listar_itens(
+    itens: list[Item] = Depends(recuperar_itens_por_pedido),
+):
     return itens
-
-
-@app.exception_handler(PedidoNaoEncontradoError)
-def tratar_erro_pedido_nao_encontrado(
-    request: Request, exc: PedidoNaoEncontradoError
-):
-    return JSONResponse(
-        status_code=HTTPStatus.NOT_FOUND,
-        content={"message": "Pedido não encontrado"},
-    )
-
-
-@app.exception_handler(FalhaDeComunicacaoError)
-def tratar_erro_falha_de_comunicacao(
-    request: Request, exc: FalhaDeComunicacaoError
-):
-    return JSONResponse(
-        status_code=HTTPStatus.BAD_GATEWAY,
-        content={"message": "Falha de comunicação com o servidor remoto"},
-    )
