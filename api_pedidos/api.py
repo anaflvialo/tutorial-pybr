95f55c58fa48b4e83607e49f0b1cdbf98fd93914
Ana Flavia Lopes <anaflvialo@gmail.com>
2022-03-03 15:47:25 -0300

    Modificacao na api

--- api_pedidos/api.py
+++ api_pedidos/api.py
@@ -1,39 +1,70 @@
 from email.policy import HTTP
-from api_pedidos.excecao import PedidoNaoEncontradoError, FalhaDeComunicacaoError
-from fastapi import FastAPI, Depends, Request
-from fastapi.responses import JSONResponse
 from http import HTTPStatus
-from api_pedidos.esquema import Item, HealthCheckResponse, ErrorResponse
 from uuid import UUID
 
+from fastapi import Depends, FastAPI, Request
+from fastapi.responses import JSONResponse
+
+from api_pedidos.esquema import ErrorResponse, HealthCheckResponse, Item
+from api_pedidos.excecao import (
+    FalhaDeComunicacaoError,
+    PedidoNaoEncontradoError,
+)
+
 app = FastAPI()
+
 
 def recuperar_itens_por_pedido(identificacao_do_pedido: UUID) -> list[Item]:
     pass
 
-@app.get('/healthcheck', tags=['healthcheck'], summary='Integridade do sistema', description='Checa se o servidor está online', response_model=HealthCheckResponse)
-async def healthcheck():
-    return HealthCheckResponse(status='ok')
 
-@app.get("/orders/{identificacao_do_pedido}/items", responses={
-    HTTPStatus.NOT_FOUND.value: {
-        "description": "Pedido não encontrado",
-        "model": ErrorResponse,
+@app.get(
+    "/healthcheck",
+    tags=["healthcheck"],
+    summary="Integridade do sistema",
+    description="Checa se o servidor está online",
+    response_model=HealthCheckResponse,
+)
+async def healthcheck():
+    return HealthCheckResponse(status="ok")
+
+
+@app.get(
+    "/orders/{identificacao_do_pedido}/items",
+    responses={
+        HTTPStatus.NOT_FOUND.value: {
+            "description": "Pedido não encontrado",
+            "model": ErrorResponse,
+        },
+        HTTPStatus.BAD_GATEWAY.value: {
+            "description": "Falha de comunicação com o servidor remoto",
+            "model": ErrorResponse,
+        },
     },
-    HTTPStatus.BAD_GATEWAY.value: {
-        "description": "Falha de comunicação com o servidor remoto",
-        "model": ErrorResponse,
-    }}, summary="Itens de um pedido", tags=["pedidos"], description="Retorna todos os itens de um determinado pedido", response_model=list[Item])
+    summary="Itens de um pedido",
+    tags=["pedidos"],
+    description="Retorna todos os itens de um determinado pedido",
+    response_model=list[Item],
+)
 def listar_items(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
     return itens
 
+
 @app.exception_handler(PedidoNaoEncontradoError)
-def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
-    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message":"Pedido não encontrado"})
+def tratar_erro_pedido_nao_encontrado(
+    request: Request, exc: PedidoNaoEncontradoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content={"message": "Pedido não encontrado"},
+    )
+
 
 @app.exception_handler(FalhaDeComunicacaoError)
-def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
-    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
-
-
-
+def tratar_erro_falha_de_comunicacao(
+    request: Request, exc: FalhaDeComunicacaoError
+):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_GATEWAY,
+        content={"message": "Falha de comunicação com o servidor remoto"},
+    )
